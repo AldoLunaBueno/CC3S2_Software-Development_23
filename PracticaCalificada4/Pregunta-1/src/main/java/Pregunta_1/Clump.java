98d946bd7a36f14a74e8504922d645506438e3e9
AldoLunaBueno <alunab@uni.pe>
2023-06-21 11:54:33 -0500

    Pregunta 2 wip

---
+++ PracticaCalificada4/Pregunta-1/src/main/java/Pregunta_1/Clump.java
@@ -0,0 +1,19 @@
+public class Clump {
+    public int countClumps(int[] nums) {
+        int countClumps = 0;
+        boolean clumpFound = false;
+        for (int i = 0; i < nums.length -1; i++) {
+            int currentValue = nums[i];
+            int nextValue = nums[i+1];
+            if (currentValue == nextValue) {
+                if (clumpFound == false) {
+                    clumpFound = true;
+                    countClumps++;
+                }
+            } else {
+                clumpFound = false;
+            }
+        }
+        return countClumps;
+    }
+}
